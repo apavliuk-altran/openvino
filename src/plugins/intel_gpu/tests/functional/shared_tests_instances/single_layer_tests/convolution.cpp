5ea33af2789f09d07d5cf08a781112707038186c
Andrii Pavliuk <andrii.pavliuk@capgemini.com>
2023-06-12 16:03:20 +0300

    [GPU] Add tests to reproduce convolution error with negative values

--- src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/convolution.cpp
+++ src/plugins/intel_gpu/tests/functional/shared_tests_instances/single_layer_tests/convolution.cpp
@@ -10,6 +10,30 @@
 using namespace LayerTestsDefinitions;
 
 namespace {
+
+class MyConvolutionLayerTest : public ConvolutionLayerTest {
+public:
+    InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo& info) const override {
+        return FuncTestUtils::createAndFillBlob(info.getTensorDesc(), 20, -10, 1, 1);
+    }
+
+protected:
+    void SetUp() override {
+        ConvolutionLayerTest::SetUp();
+
+        auto params = this->GetParam();
+        auto netPrecision = std::get<1>(params);
+        if (netPrecision.getPrecVal() == InferenceEngine::Precision::FP16) {
+            this->threshold = 0.5;
+        }
+    }
+};
+
+TEST_P(MyConvolutionLayerTest, CompareWithRefs) {
+    SKIP_IF_CURRENT_TEST_IS_DISABLED()
+    Run();
+}
+
 const std::vector<InferenceEngine::Precision> netPrecisions = {
         InferenceEngine::Precision::FP32,
         InferenceEngine::Precision::FP16
@@ -108,4 +132,57 @@ INSTANTIATE_TEST_SUITE_P(smoke_Convolution3D_Basic1, ConvolutionLayerTest,
                                  ::testing::Values(std::vector<size_t >({1, 3, 10, 10, 10})),
                                  ::testing::Values(CommonTestUtils::DEVICE_GPU)),
                          ConvolutionLayerTest::getTestCaseName);
+
+
+/* ======================================================================== */
+
+// Attrs:  {'auto_pad': 'explicit', 'dilations': '1', 'pads_begin': '2', 'pads_end': '2', 'strides': '1'}
+// In:     (1, 512, 1000), (80, 512, 5)
+// Out:    (1, 80, 1000)
+// Operators: 'Tacotron2-graph-transform-cuda-postnet:opid22' [FP32], 'Tacotron2-postnet:opid22' [FP32]
+INSTANTIATE_TEST_CASE_P(
+    Convolution_Tacotron2_graph_transform_cuda_postnet_opid22,
+    MyConvolutionLayerTest,
+    ::testing::Combine(
+        ::testing::Combine(
+            ::testing::Values(std::vector<size_t>({5})), // kernel
+            ::testing::Values(std::vector<size_t>({1})), // strides
+            ::testing::Values(std::vector<ptrdiff_t>({2})), // pads_begin
+            ::testing::Values(std::vector<ptrdiff_t>({2})), // pads_end
+            ::testing::Values(std::vector<size_t>({1})), // dilations
+            ::testing::Values(80), // Num out channels
+            ::testing::Values(ov::op::PadType::EXPLICIT)), // Padding type
+        ::testing::ValuesIn(std::vector<InferenceEngine::Precision>({InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16})), // Net precisions
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED), // Input precision
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED), // Output precision
+        ::testing::Values(InferenceEngine::Layout::ANY), // Input layout
+        ::testing::Values(InferenceEngine::Layout::ANY), // Output layout
+        ::testing::Values(std::vector<size_t>({1, 512, 1000})), // Input shape
+        ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+    MyConvolutionLayerTest::getTestCaseName);
+
+// Attrs:  {'auto_pad': 'explicit', 'dilations': '1', 'pads_begin': '0', 'pads_end': '0', 'strides': '1'}
+// In:     (1, 16, 32), (8, 16, 5)
+// Out:    (1, 8, 32)
+INSTANTIATE_TEST_CASE_P(
+    tst001,
+    MyConvolutionLayerTest,
+    ::testing::Combine(
+        ::testing::Combine(
+            ::testing::Values(std::vector<size_t>({5})), // kernel
+            ::testing::Values(std::vector<size_t>({1})), // strides
+            ::testing::Values(std::vector<ptrdiff_t>({0})), // pads_begin
+            ::testing::Values(std::vector<ptrdiff_t>({0})), // pads_end
+            ::testing::Values(std::vector<size_t>({1})), // dilations
+            ::testing::Values(8), // Num out channels
+            ::testing::Values(ov::op::PadType::EXPLICIT)), // Padding type
+        ::testing::ValuesIn(std::vector<InferenceEngine::Precision>({InferenceEngine::Precision::FP32, InferenceEngine::Precision::FP16})), // Net precisions
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED), // Input precision
+        ::testing::Values(InferenceEngine::Precision::UNSPECIFIED), // Output precision
+        ::testing::Values(InferenceEngine::Layout::ANY), // Input layout
+        ::testing::Values(InferenceEngine::Layout::ANY), // Output layout
+        ::testing::Values(std::vector<size_t>({1, 16, 32})), // Input shape
+        ::testing::Values(CommonTestUtils::DEVICE_GPU)),
+    MyConvolutionLayerTest::getTestCaseName);
+
 }  // namespace
